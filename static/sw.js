96057dfe46b9ddaad8217512e24c8f90057ba83c
satyam0507 <satyam0507@gmail.com>
2017-03-08 23:28:46 +0530

    cache first option added

--- static/sw.js
+++ static/sw.js
@@ -28,7 +28,7 @@ var config = {
         'compiled/js/main.js',
         '/anything'
     ],
-    defaultHandler: "fastest",
+    defaultHandler: "cacheFirst",
     navigationFallBack:'/anything'
 }
 // console.log('i am service worker');
